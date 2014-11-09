02e9ae52b6585d5d4a71865f17678cd6f0bedc8f
Federico Tomassetti <f.tomassetti@gmail.com>
2014-11-09 17:24:43 +0000

    Working on rotation

--- src/Main.hs
+++ src/Main.hs
@@ -11,20 +11,21 @@ import Graphics.UI.GLUT
 import qualified Data.Map.Strict as M
 import Data.Foldable (toList)
 import Data.Maybe
+import Data.IORef
 
 type MColor = (GLfloat,GLfloat,GLfloat)
 type GLPoint = (GLfloat,GLfloat,GLfloat)
 type ColoredPoint = (MColor, GLPoint)
 type ColoredQuad = (ColoredPoint, ColoredPoint, ColoredPoint, ColoredPoint)
 
-toGlCoords :: Int -> Int -> IPoint -> (GLfloat,GLfloat,GLfloat)
-toGlCoords width height (x,y) = let xf :: Float = fromIntegral x
-                                    yf :: Float = fromIntegral y
-                                    wf :: Float = fromIntegral width
-                                    hf :: Float = fromIntegral height
-                                    glX :: GLfloat = realToFrac $ ((xf / wf) * 2.0) - 1.0
-                                    glY :: GLfloat = realToFrac $ ((yf / hf) * 2.0) - 1.0
-                                in (glX, glY, 0.5)
+toGlCoords :: Int -> Int -> (IPoint,Float) -> (GLfloat,GLfloat,GLfloat)
+toGlCoords width height ((x,y),elev) = let xf :: Float = fromIntegral x
+                                           yf :: Float = fromIntegral y
+                                           wf :: Float = fromIntegral width
+                                           hf :: Float = fromIntegral height
+                                           glX :: GLfloat = realToFrac $ ((xf / wf) * 2.0) - 1.0
+                                           glY :: GLfloat = realToFrac $ ((yf / hf) * 2.0) - 1.0
+                                       in (glX, glY, realToFrac $ elev - 1.0)
 
 landColor :: MColor
 landColor = (realToFrac 0.0, realToFrac 1.0, realToFrac 0.0)
@@ -32,24 +33,24 @@ landColor = (realToFrac 0.0, realToFrac 1.0, realToFrac 0.0)
 oceanColor :: MColor
 oceanColor = (realToFrac 0.0, realToFrac 0.0, realToFrac 1.0)
 
-toColoredPoints :: Int -> Int -> [IPoint] -> MColor -> [(MColor,(GLfloat,GLfloat,GLfloat))]
-toColoredPoints width height coords color = let glCoords = map (toGlCoords width height) coords
-                                            in zip (repeat color) glCoords
+--toColoredPoints :: Int -> Int -> [IPoint] -> MColor -> [(MColor,(GLfloat,GLfloat,GLfloat))]
+--toColoredPoints width height coords color = let glCoords = map (toGlCoords width height) coords
+--                                            in zip (repeat color) glCoords
 
-findPoints :: ElevationMap -> Int -> Int -> [(MColor,(GLfloat,GLfloat,GLfloat))]
-findPoints elevMap width height = let values :: [(IPoint,Float)] = M.toList elevMap
-                                      landValues  :: [(IPoint,Float)] = filter (\(_,e) -> e>0.5) values
-                                      oceanValues :: [(IPoint,Float)] = filter (\(_,e) -> e<=0.5) values
-                                      landCoords  :: [IPoint] = map fst landValues
-                                      oceanCoords  :: [IPoint] = map fst oceanValues
-                                  in toColoredPoints width height landCoords landColor ++ toColoredPoints width height oceanCoords oceanColor
+--findPoints :: ElevationMap -> Int -> Int -> [(MColor,(GLfloat,GLfloat,GLfloat))]
+--findPoints elevMap width height = let values :: [(IPoint,Float)] = M.toList elevMap
+--                                      landValues  :: [(IPoint,Float)] = filter (\(_,e) -> e>0.5) values
+--                                      oceanValues :: [(IPoint,Float)] = filter (\(_,e) -> e<=0.5) values
+--                                      landCoords  :: [IPoint] = map fst landValues
+--                                      oceanCoords  :: [IPoint] = map fst oceanValues
+--                                  in toColoredPoints width height landCoords landColor ++ toColoredPoints width height oceanCoords oceanColor
 
 toColoredPoint :: ElevationMap -> Int -> Int -> IPoint -> ColoredPoint
 toColoredPoint elevationMap width height p@(x,y) = let elev = case M.lookup (x,y) elevationMap of
                                                               Just e -> e
                                                               Nothing -> error $ "Not found point "++(show x)++", "++(show y)++" in elevation map"
                                                        color = if elev<0.5 then oceanColor else landColor
-                                                       glCoords = toGlCoords width height p
+                                                       glCoords = toGlCoords width height (p,elev)
                                                    in (color,glCoords)
 
 findQuads :: ElevationMap -> Int -> Int -> [ColoredQuad]
@@ -113,16 +114,46 @@ drawColoredQuad (a,b,c,d) = do drawColoredPoint a
                                drawColoredPoint d
 
 
-display :: ElevationMap -> Int -> Int -> DisplayCallback
-display elevMap width height = do
-  clear [ ColorBuffer ]
-  let points = findPoints elevMap width height
+display :: ElevationMap -> Int -> Int -> IORef GLfloat -> IORef (GLfloat, GLfloat) -> DisplayCallback
+display elevMap width height angle pos = do
   let quads  = findQuads  elevMap width height
+  clear [ ColorBuffer, DepthBuffer ]
+  clear [ ColorBuffer ]
+  loadIdentity
+  (x',y') <- get pos
+  translate $ Vector3 x' y' 0
+  preservingMatrix $ do
+      a <- get angle
+      rotate a $ Vector3 0 1 0
+      rotate a $ Vector3 0.1 1 0 -- changed y-component a bit to show off cube corners
+      scale 0.7 0.7 (0.7::GLfloat)
+      preservingMatrix $ do
+        renderPrimitive Quads $
+            mapM_ drawColoredQuad quads
+  swapBuffers
   --renderPrimitive Points $
   --     mapM_ drawColoredPoint points
-  renderPrimitive Quads $
-       mapM_ drawColoredQuad quads
+
   flush
+
+keyboardMouse :: IORef GLfloat -> IORef (GLfloat, GLfloat) -> KeyboardMouseCallback
+keyboardMouse a p key Down _ _ = case key of
+  (Char ' ') -> a $~! negate
+  (Char '+') -> a $~! (* 2)
+  (Char '-') -> a $~! (/ 2)
+  (SpecialKey KeyLeft ) -> p $~! \(x,y) -> (x-0.1,y)
+  (SpecialKey KeyRight) -> p $~! \(x,y) -> (x+0.1,y)
+  (SpecialKey KeyUp   ) -> p $~! \(x,y) -> (x,y+0.1)
+  (SpecialKey KeyDown ) -> p $~! \(x,y) -> (x,y-0.1)
+  _ -> return ()
+keyboardMouse _ _ _ _ _ _ = return ()
+
+
+idle :: IORef GLfloat -> IORef GLfloat -> IdleCallback
+idle angle delta = do
+  d <- get delta
+  angle $~! (+ d)
+  postRedisplay Nothing
 
 main = do
     world <- loadWorld worldPath
@@ -131,7 +162,12 @@ main = do
         height  = worldHeight world
     (_progName, _args) <- getArgsAndInitialize
     _window <- createWindow "World Explorer"
-    displayCallback $= display elevMap width height
+    angle <- newIORef 0.3
+    delta <- newIORef 0.1
+    pos <- newIORef (0, 0)
+    keyboardMouseCallback $= Just (keyboardMouse delta pos)
+    idleCallback $= Just (idle angle delta)
+    displayCallback $= display elevMap width height angle pos
     mainLoop
 
 
